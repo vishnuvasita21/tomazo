61af9e4c20f9d1b71f6ebab9c1cff28514cb955e
Deepti <deeptituteja1911@gmail.com>
2023-11-22 23:17:19 -0400

    rating and reviews

---
+++ Customer App/backend/review/ddb.helper.mjs
@@ -0,0 +1,67 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  ScanCommand,
+  PutCommand,
+  DeleteCommand,
+  ExecuteStatementCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({ region: "us-east-1" });
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+const tableName = "Reviews";
+
+export const putItem = async (item) => {
+  const putCommand = new PutCommand({
+    TableName: tableName,
+    Item: item,
+  });
+  const response = await ddbDocClient.send(putCommand);
+  console.log("put response", response);
+};
+
+export const getItem = async (id) => {
+  const getCommand = new GetCommand({
+    TableName: tableName,
+    Key: {
+      id,
+    },
+    ConsistentRead: true,
+  });
+  const getResponse = await ddbDocClient.send(getCommand);
+
+  console.log("reponse", getResponse);
+};
+
+export const getAllItems = async () => {
+  const getCommand = new ScanCommand({
+    TableName: tableName,
+  });
+  const getResponse = await ddbDocClient.send(getCommand);
+
+  console.log("reponse", getResponse);
+};
+
+export const getItemByRestaurant = async (id) => {
+  const getCommand = new ExecuteStatementCommand({
+    Statement: `SELECT * FROM ${tableName} WHERE id=? AND type=?`,
+    Parameters: [`${id}`, "RESTAURANT"],
+    ConsistentRead: true,
+
+  });
+  const getResponse = await ddbDocClient.send(getCommand);
+
+  console.log("reponse", getResponse);
+};
+
+export const deleteItem = async (item) => {
+  const deleteCommand = new DeleteCommand({
+    TableName: tableName,
+    Key: item,
+  });
+  const response = await client.send(deleteCommand);
+  console.log("delete response", response);
+};
+
+// module.exports = { putItem, getItem, getAllItems, deleteItem };
