4bf4384bc3e05db6d8b9f4c5a1a7126ea2790154
Deepti <deeptituteja1911@gmail.com>
2023-12-02 02:32:23 -0400

    update reservation status

---
+++ Partner App/backend/chatbot/updateReservationStatus.py
@@ -0,0 +1,49 @@
+import json
+import requests
+
+def lambda_handler(event, context):
+    try:
+        # First API Endpoint
+        url_get_document_id = "https://us-central1-csci5410-14dd5.cloudfunctions.net/getReservationDocumentID"
+
+        # Making the POST request to the first API
+        response_get_document_id = requests.post(url_get_document_id, json=event)
+
+        # Check if the request was successful
+        if response_get_document_id.status_code != 200:
+            return {
+                "statusCode": response_get_document_id.status_code,
+                "body": "Error fetching document ID: " + response_get_document_id.text
+            }
+
+        # Extracting the documentID from the response
+        document_id = response_get_document_id.json()["documentID"]
+
+        # Second API Endpoint with documentID and bookingStatus as query parameters
+        url_update_reservation = f"https://us-central1-csci5410-14dd5.cloudfunctions.net/updateReservationRestaurant?documentID={document_id}&bookingStatus=Approved"
+
+        # Making the GET request to the second API
+        response_update_reservation = requests.get(url_update_reservation)
+
+        # Check if the update request was successful
+        if response_update_reservation.status_code != 200:
+            return {
+                "statusCode": response_update_reservation.status_code,
+                "body": "Error in updating reservation: " + response_update_reservation.text
+            }
+
+        # Returning the success response
+        return {
+            "statusCode": 200,
+            "body": "Reservation updated successfully"
+        }
+
+    except Exception as e:
+        # Log the exception for debugging purposes
+        print(f"Error: {str(e)}")
+
+        # Return a custom error message
+        return {
+            "statusCode": 500,
+            "body": "An error occurred: " + str(e)
+        }
