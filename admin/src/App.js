3b38c4616784f9e454073ec2371a4b048fd6ac72
JASMEET SINGH <jasmeets>
2023-12-02 22:49:29 -0400

    adding login page for admin app

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import image from "../src/Assets/login-gov-600x314.png";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AdminLogin from "./views/login/AdminLogin";
+
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <div className="left">
+        <img
+          src={image}
+          style={{ width: "80%", maxWidth: "100%", height: "auto" }}
+          alt="Loading..."
+        />
+      </div>
+      <div className="right">
+        <div className="content">
+          <h1>Admin Panel</h1>
+          <Link to="/login">
+            <button className="red-button">Login</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<AdminLogin />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ admin/src/views/login/AdminLogin.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { TextField, Button, Box, Typography } from "@mui/material";
+import "./AdminLogin.css";
+
+function AdminLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isValidPassword, setIsValidPassword] = useState(true);
+
+  const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    return password.length >= 6; // Basic password length validation
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validEmail = validateEmail(email);
+    setIsValidEmail(validEmail);
+    const validPassword = validatePassword(password);
+    setIsValidPassword(validPassword);
+
+    if (validEmail && validPassword) {
+        console.log("Email:", email);
+        console.log("Password:", password);
+      // Here you can add logic for login authentication
+    }
+  };
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Box className="login-box" component="form" onSubmit={handleSubmit}>
+        <Typography variant="h5" align="center" gutterBottom>
+          Login
+        </Typography>
+        <TextField
+          label="Email"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          error={!isValidEmail}
+          helperText={!isValidEmail && "Enter a valid email"}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          fullWidth
+          margin="normal"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          error={!isValidPassword}
+          helperText={
+            !isValidPassword &&
+            "Password should be at least 6 characters long"
+          }
+        />
+        <Button variant="contained" color="primary" fullWidth type="submit">
+          Login
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default AdminLogin;
